1d0f1875ae58b29214b182628838408ed587fe25
sazzycooltehc <aravindsas999@gmail.com>
2025-05-15 07:06:36 +0530

    IM: Commit - MERN app - header

--- Fullstack-RN/client/src/App.js
+++ Fullstack-RN/client/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TodoForm from './Todoform';
+import Header from './shared/header';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -28,19 +28,7 @@ const App = () => {
 
   return (
     <div className='App'>
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      <Header/>
       <h1>MERN Stack App</h1>
       <TodoForm onAdd={addTodo} />
       <ul>
