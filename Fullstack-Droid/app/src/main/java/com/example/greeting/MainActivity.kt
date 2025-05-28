776987935dd744c46e467c08c4deb5631cdccb76
sazzycooltehc <aravindsas999@gmail.com>
2025-05-28 22:12:43 +0530

    IM: Android app template

--- Fullstack-Droid/app/src/main/java/com/example/greeting/MainActivity.kt
+++ Fullstack-Droid/app/src/main/java/com/example/greeting/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greeting.ui.theme.GreetingTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +35,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Magenta) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreetingTheme {
-        Greeting("Android")
+        Greeting("Oreo")
     }
 }
