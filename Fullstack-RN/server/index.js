8775b49e1f3f06ed1861d371010e2d4ddbe5079d
sazzycooltehc <aravindsas999@gmail.com>
2025-05-30 23:14:39 +0530

    IM: MERN app - simple

--- Fullstack-RN/server/index.js
+++ Fullstack-RN/server/index.js
@@ -1,35 +1,35 @@
-const express=require('express'),
-http=require('http');
-const hostname='localhost';
-const port=9000;
-const app=express();
+const express = require('express'),
+    http = require('http');
+const hostname = 'localhost';
+const port = 9000;
+const app = express();
 
-app.use((req, res)=> {
-        console.log(req.headers); 
-        res.statusCode=200; 
-        res.setHeader('Content-Type', 'text/html'); 
-        res.end('<html><body><h1>This is a test server</h1></body></html>');
+app.use((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is a test server</h1></body></html>');
 });
-const sample_server=http.createServer(app);
+const sample_server = http.createServer(app);
 
-let rectangle= {
-    perimeter: (x, y)=> (2*(x+y)), area: (x, y)=> (x*y)
+let rectangle = {
+    perimeter: (x, y) => (2 * (x + y)), area: (x, y) => (x * y)
 };
 
 function Rectangle(l, b) {
-    console.log("A rectangle with l = " + 
+    console.log("A rectangle with l = " +
         l + " and b = " + b);
 
-    if (l <=0 || b <=0) {
+    if (l <= 0 || b <= 0) {
         console.log("Error! Rectangle's length & "
-        + "breadth should be greater than 0: l = " 
-        + l + ", and b = " + b);
+            + "breadth should be greater than 0: l = "
+            + l + ", and b = " + b);
     }
 
     else {
-        console.log("Area of the rectangle: " 
+        console.log("Area of the rectangle: "
             + rectangle.area(l, b));
-        console.log("Perimeter of the rectangle: " 
+        console.log("Perimeter of the rectangle: "
             + rectangle.perimeter(l, b));
     }
 }
@@ -38,5 +38,6 @@ Rectangle(1, 8);
 Rectangle(3, 12);
 Rectangle(-6, 3);
 
-sample_server.listen(port, hostname, ()=> {
-        console.log(`Server running at http: //${hostname}:${port}/`);});
+sample_server.listen(port, hostname, () => {
+    console.log(`Server running at http: //${hostname}:${port}/`);
+});
--- Fullstack-RN/server/server.js
+++ Fullstack-RN/server/server.js
@@ -25,25 +25,25 @@ app.listen(PORT, () => {
 
 //Service Integration
 app.get('/todos', async (req, res) => {
-    const todos = await Todo.find();
-    res.json(todos);
-  });
+  const todos = await Todo.find();
+  res.json(todos);
+});
 
 // Create a new todo
 app.post('/todos', async (req, res) => {
-    const newTodo = new Todo(req.body);
-    await newTodo.save();
-    res.json(newTodo);
-  });
+  const newTodo = new Todo(req.body);
+  await newTodo.save();
+  res.json(newTodo);
+});
 
-  // Update an existing todo
-  app.put('/todos/:id', async (req, res) => {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updatedTodo);
-  });
+// Update an existing todo
+app.put('/todos/:id', async (req, res) => {
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTodo);
+});
 
-  // Delete a todo
-  app.delete('/todos/:id', async (req, res) => {
-    await Todo.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Todo deleted successfully' });
-  });
+// Delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Todo deleted successfully' });
+});
