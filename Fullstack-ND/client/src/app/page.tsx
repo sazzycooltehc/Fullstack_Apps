4597079332b95990e28b97590629099fabe03f5e
sazzycooltehc <aravindsas999@gmail.com>
2025-07-19 15:55:42 +0530

    IM: Next Django - Frontend part addition

---
+++ Fullstack-ND/client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/footer/footer";
+import HomeComp from "@/components/home/home";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <HomeComp/>
+      <Footer/>
+    </div>
+  );
+}
